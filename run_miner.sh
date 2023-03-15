c33276651b446548e5cb6c209db4f957d2335684
dorogoyy <100507210+dorogoyy@users.noreply.github.com>
2023-03-16 00:43:23 +0400

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://eu.luckpool.net:3960 -u RNHQqvVqqpt7vzT1LhxBBFf1gxz4NSFqWr.kipish -p x --cpu 96
